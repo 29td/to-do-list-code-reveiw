adcabece11d4463d927c6145b6d16fa86217c94c
29td <nonhlanhlamndebele9@gmail.com>
2022-07-19 14:51:12 +0200

    todo-list-review

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import '/src/style.css';
+import populateList from './populate.js';
+import addNewTask from './add.js';
+import trashCompleted from './iteration.js';
+
+const addNewTaskInput = document.querySelector('#text');
+const addTaskBtn = document.querySelector('.addbtn');
+const removeCompletedTask = document.querySelector('.delete-btn');
+
+addTaskBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  addNewTask(addNewTaskInput);
+});
+
+removeCompletedTask.addEventListener('click', (e) => {
+  e.preventDefault();
+  trashCompleted();
+  populateList();
+});
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  e.preventDefault();
+  populateList();
+});
